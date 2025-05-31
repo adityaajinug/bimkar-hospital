74338f20d0155bf2cc007cfdb16f5e3206b87cb4
Aditya Ajie Nugroho <adityaajinug@gmail.com>
2025-05-31 10:27:28 +0700

    feat: obat relation

--- app/Models/Obat.php
+++ app/Models/Obat.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Obat extends Model
 {
@@ -12,4 +13,9 @@ class Obat extends Model
     protected $fillable = [
         'nama_obat', 'kemasan', 'harga'
     ];
+
+    public function detailPeriksas(): HasMany 
+    { 
+        return $this->hasMany(DetailPeriksa::class, 'id_obat'); 
+    } 
 }
