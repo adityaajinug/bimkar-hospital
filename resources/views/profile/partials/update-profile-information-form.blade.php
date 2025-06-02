a04497a9eb9984ea829d8df7faea40a165768812
Aditya Ajie Nugroho <adityaajinug@gmail.com>
2025-06-02 11:38:25 +0700

    fix: update conditio for dokter

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -22,11 +22,6 @@
             <x-text-input id="name" name="no_rm" type="text" class="mt-1 block w-full" :value="old('No RM', $user->no_rm)" required autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('no_rm')" />
         </div>
-        <div>
-            <x-input-label for="poli" :value="__('Poli')" />
-            <x-text-input id="poli" name="poli" type="text" class="mt-1 block w-full" :value="old('poli', $user->poli)" required autofocus autocomplete="poli" />
-            <x-input-error class="mt-2" :messages="$errors->get('poli')" />
-        </div>
         @endif
         <div>
             <x-input-label for="name" :value="__('Nama')" />
@@ -50,6 +45,15 @@
             <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
         </div>
+
+        @if($user->role == 'dokter')
+         <div>
+            <x-input-label for="poli" :value="__('Poli')" />
+            <x-text-input id="poli" name="poli" type="text" class="mt-1 block w-full" :value="old('poli', $user->poli)" required autofocus autocomplete="poli" />
+            <x-input-error class="mt-2" :messages="$errors->get('poli')" />
+        </div>
+        @endif
+        
          <div>
             <x-input-label for="alamat" :value="__('Alamat')" />
             <x-text-input id="alamat" name="alamat" type="text" class="mt-1 block w-full" :value="old('alamat', $user->alamat)" required autofocus autocomplete="alamat" />
